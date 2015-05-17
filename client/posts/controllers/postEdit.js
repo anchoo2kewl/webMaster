fc09ad34b446a4b72b6295fb54e5bdf87edf24ba
Anshuman Biswas <hi@anshumanbiswas.com>
2015-05-16 22:44:37 -0400

    added date functionality

--- client/posts/controllers/postEdit.js
+++ client/posts/controllers/postEdit.js
@@ -1,9 +1,7 @@
 angular.module("main").controller("PostEditCtrl", ['$scope', '$stateParams', '$meteor',
   function($scope, $stateParams, $meteor){
 
-
-    // $scope.post.created = new Date();
-    // $scope.post.updated = new Date();
+    $scope.date = new Date();
     $scope.post = $meteor.object(Posts, $stateParams.postsId, false); 
 
     var subscriptionHandle;
@@ -14,11 +12,8 @@ angular.module("main").controller("PostEditCtrl", ['$scope', '$stateParams', '$m
     $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
 
     $scope.save = function() {
-      $scope.post.created = new Date();
       $scope.post.updated = new Date();
 
-      console.log($scope.post.created);
-      console.log($scope.post);
     $scope.post.save().then(function(numberOfDocs){
       console.log('save success doc affected ', numberOfDocs);
       }, function(error){
